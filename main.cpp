c96c6939faec5f32867cdb958ff851262012310d
AmiQuitO <amiquitooriginal@gmail.com>
2022-08-30 20:50:51 +0200

    working sand water solid air, cursor
    water still needs some polishing

--- main.cpp
+++ main.cpp
@@ -8,23 +8,140 @@ void WorldDraw() {
 	}
 }
 
+void WorldGenerate() {
+	for (int i = 0; i < GAMEHEIGHT; i++) {
+		for (int j = 0; j < GAMEWIDTH; j++) {
+			particles[i][j].SetPosition(Vector2{ (float)j, (float)i });
+			particles[i][j].SetColor(BLACK);
+		}
+	}
+}
+
+void WorldMove() { // priority sand > water > air
+	for (int i = GAMEHEIGHT; i > 0; i--) {
+		for (int j = GAMEWIDTH; j > 0; j--) {
+			// sand
+			if (particles[i][j].IsColor(YELLOW)) {
+
+				if (particles[i][j].IsColor(YELLOW) && particles[i + 1][j].IsColor(BLACK) && i < GAMEHEIGHT) { // some sand can get off screen what?
+					particles[i][j].SetColor(BLACK);
+					particles[i + 1][j].SetColor(YELLOW);
+				}
+				else if (particles[i][j].IsColor(YELLOW) && particles[i + 1][j - 1].IsColor(BLACK) && i < GAMEHEIGHT && j < GAMEWIDTH - 1 && j>0) { // left
+					particles[i][j].SetColor(BLACK);
+					particles[i + 1][j - 1].SetColor(YELLOW);
+				}
+				else if (particles[i][j].IsColor(YELLOW) && particles[i + 1][j + 1].IsColor(BLACK) && i < GAMEHEIGHT && j < GAMEWIDTH - 1) { // right
+					particles[i][j].SetColor(BLACK);
+					particles[i + 1][j + 1].SetColor(YELLOW);
+				}
+				else if (particles[i][j].IsColor(YELLOW) && particles[i + 1][j].IsColor(BLUE) && i < GAMEHEIGHT) {
+					particles[i][j].SetColor(BLUE);
+					particles[i + 1][j].SetColor(YELLOW);
+				}
+				else if (particles[i][j].IsColor(YELLOW) && particles[i + 1][j - 1].IsColor(BLUE) && i < GAMEHEIGHT && j < GAMEWIDTH - 1 && j>0) { // left
+					particles[i][j].SetColor(BLUE);
+					particles[i + 1][j - 1].SetColor(YELLOW);
+				}
+				else if (particles[i][j].IsColor(YELLOW) && particles[i + 1][j + 1].IsColor(BLUE) && i < GAMEHEIGHT && j < GAMEWIDTH - 1) { // right
+					particles[i][j].SetColor(BLUE);
+					particles[i + 1][j + 1].SetColor(YELLOW);
+				}
+			}
+			// water
+			if (particles[i][j].IsColor(BLUE)) {
+				if (particles[i][j].IsColor(BLUE) && particles[i + 1][j].IsColor(BLACK) && i < GAMEHEIGHT) {
+					particles[i][j].SetColor(BLACK);
+					particles[i + 1][j].SetColor(BLUE);
+				}
+				for (int k = 0; k < GAMEWIDTH; k++) {
+					if (particles[i][j].IsColor(BLUE) && particles[i + 1][j - k].IsColor(BLACK) && i < GAMEHEIGHT && j < GAMEWIDTH - 1 && j>0) { // left
+						particles[i][j].SetColor(BLACK);
+						particles[i + 1][j - k].SetColor(BLUE);
+					}
+					else if (particles[i][j].IsColor(BLUE) && particles[i + 1][j + k].IsColor(BLACK) && i < GAMEHEIGHT && j < GAMEWIDTH - 1) { // right
+						particles[i][j].SetColor(BLACK);
+						particles[i + 1][j + k].SetColor(BLUE);
+					}
+				}
+			}
+		}
+	}
+}
 
 int main() {
 	InitWindow(800, 800, "Sandbox");
 	SetWindowState(FLAG_VSYNC_HINT);
 	SetTargetFPS(60);
 
-	while (!WindowShouldClose()) {
+	WorldGenerate();
 
+	while (!WindowShouldClose()) {
+		
+		mousePos = GetMousePosition();
+
+		// drawing 
+		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+			if (mousePos.x < WINDOWWIDTH && mousePos.y < WINDOWHEIGHT && mousePos.x > 0 && mousePos.y > 0) {
+				for (int i = 1 - scrollY; i < scrollY; i++) {
+					for (int j = 1 - scrollY; j < scrollY; j++) {
+						if (floor(mousePos.x) + j * PARTICLESIZE < WINDOWWIDTH && floor(mousePos.y) + i * PARTICLESIZE < WINDOWHEIGHT
+							&& floor(mousePos.x) - i * PARTICLESIZE > 0 && floor(mousePos.y) - i * PARTICLESIZE > 0) {
+							particles[(int)(mousePos.y / PARTICLESIZE) + i][(int)(mousePos.x / PARTICLESIZE) + j].SetColor(typeC);
+						}
+					}
+				}
+			}
+		}
+
+		// changing type
+		if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
+			type++;
+			if (type > 3) {
+				type = 0;
+			}
+
+			if (type == 0) {
+				typeC = BLACK;
+				typeN = "Air";
+			}
+			else if (type == 1) {
+				typeC = YELLOW;
+				typeN = "Sand";
+			}
+			else if (type == 2) {
+				typeC = BLUE;
+				typeN = "Water";
+			}
+			else if (type == 3) {
+				typeC = GRAY;
+				typeN = "Solid";
+			}
+		}
+
+		// scrolling 
+		scrollY += GetMouseWheelMove();
+
+		if (scrollY < 1) {
+			scrollY = 1;
+		}
 
 		BeginDrawing();
-		ClearBackground(BLACK);
+		ClearBackground(WHITE);
 
-		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-			Vector2 mousePos = GetMousePosition();
-			particles[(int)mousePos.y][(int)mousePos.x].SetColor(YELLOW); // sand for now
-		}
+		WorldMove();
+
 		WorldDraw();
+
+		// cursor
+		for (int i = 1 - scrollY; i < scrollY; i++) {
+			for (int j = 1 - scrollY; j < scrollY; j++) {
+				if (i == 1 - scrollY || i == scrollY - 1 || j == 1 - scrollY || j == scrollY - 1) {
+					DrawRectangle(mousePos.x + PARTICLESIZE * i, mousePos.y + PARTICLESIZE * j, PARTICLESIZE, PARTICLESIZE, WHITE);
+				}
+			}
+		}
+		DrawText(typeN.c_str(), mousePos.x - MeasureText(typeN.c_str(), 20)/2, mousePos.y-20, 20, typeC);
 
 		DrawFPS(0, 0);
 		EndDrawing();
--- main.h
+++ main.h
@@ -3,29 +3,45 @@
 #include "raylib.h"
 #include <string>
 #include <cmath>
+#include <string>
 
 
-const int GAMEWIDTH = 40;
-const int GAMEHEIGHT = 40;
+const int GAMEWIDTH = 160;
+const int GAMEHEIGHT = 160;
 
-const int PARTICLESIZE = 20;
+const int WINDOWWIDTH = 800;
+const int WINDOWHEIGHT = 800;
+
+const int PARTICLESIZE = 5;
+
+Vector2 mousePos;
+int scrollY = 1;
+
+int type = 1; //0 = air, 1 = sand, 2 = water, 3 = solid
+Color typeC = YELLOW;
+std::string typeN = "Sand";
 
 class Particle {
 private:
 	Vector2 position;
 	Color color;
+	bool hasMoved = false;
 
 public:
-	Particle() {
-		color = BLACK;
+	void SetPosition(Vector2 pos) {
+		position = pos;
 	}
 
 	void SetColor(Color c) {
 		color = c;
 	}
 
+	bool IsColor(Color c) {
+		return color.r == c.r && color.g == c.g && color.b == c.b;
+	}
+
 	void Draw() {
-		DrawRectangle((int)position.x * 20, (int)position.y * 20, PARTICLESIZE, PARTICLESIZE, color);
+		DrawRectangle((int)position.x * PARTICLESIZE, (int)position.y * PARTICLESIZE, PARTICLESIZE, PARTICLESIZE, color);
 	}
 };
 Particle particles[GAMEHEIGHT][GAMEWIDTH];
